4b7238dc26bc97324e796ea75674fce5edf0b66d
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-14 19:27:50 +0200

    In search user card for now is done with styles.

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,11 +1,12 @@
 import ProfileHeader from "@/components/shared/ProfileHeader";
-import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import Image from "next/image";
 import { profileTabs } from "@/constants";
 import ConvergsTab from "@/components/shared/ConvergsTab";
+import UserCard from "@/components/cards/UserCard";
 
 const Page = async () => {
   const user = await currentUser();
@@ -15,9 +16,38 @@ const Page = async () => {
   const userInfo = await fetchUser(user.id);
 
   if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+
+      {/* Search Bar */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 };
